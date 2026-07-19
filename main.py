5a1089bfc7ddc763278b3831d0f72f1ab02b7216
Ian <6markian7@gmail.com>
2026-07-19 07:21:01 +0300

    final code with approaches to all the SQL queries satisfying (filtering, ordering, limiting, grouping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sqlite3
 
 ##### Part I: Basic Filtering #####
@@ -12,25 +13,78 @@ pd.read_sql("""SELECT * FROM planets; """, conn1)
 
 # STEP 1
 # Replace None with your code
-df_no_moons = None
+df_no_moons = pd.read_sql("""
+        SELECT * 
+        FROM planets
+        WHERE num_of_moons = 0
+
+""", conn1)
+#print(df_no_moons)
 
 # STEP 2
 # Replace None with your code
-df_name_seven = None
+""" WITH DYNAMISM """
+#name_length = int(input("Enter name length to search: "))
+# df_name_seven = pd.read_sql("""
+#         SELECT name, mass
+#         FROM planets
+#         WHERE length(name) = ?
+
+# """, conn1, params=(name_length,))
+#print(df_name_seven)
+
+""" HARDCODED """
+df_name_seven = pd.read_sql("""
+        SELECT name, mass
+        FROM planets
+        WHERE length(name) = 7
+
+""", conn1)
 
 ##### Part 2: Advanced Filtering #####
 
 # STEP 3
 # Replace None with your code
-df_mass = None
+""" WITH DYNAMISM """
+# size=float(input("Enter mass (less than or equal to) to search: "))
+# df_mass = pd.read_sql("""
+#         SELECT name, mass
+#         FROM planets
+#         WHERE mass <= ?
+
+# """, conn1, params=(size,))
+#print(df_mass)
+
+""" HARDCODED """
+df_mass = pd.read_sql("""
+        SELECT name, mass
+        FROM planets
+        WHERE mass <= 1
+
+""", conn1)
+#print(df_mass)
 
 # STEP 4
 # Replace None with your code
-df_mass_moon = None
+#moon_nums=int(input("Enter number of moons to search: "))
+#size=float(input("Enter mass (less than) to search: "))
+df_mass_moon = pd.read_sql("""
+        SELECT *
+        FROM planets
+        WHERE num_of_moons >=1 AND mass < 1
+                           
+""",conn1)
+#print(df_mass_moon)
 
 # STEP 5
 # Replace None with your code
-df_blue = None
+df_blue = pd.read_sql("""
+        SELECT name, color
+        FROM planets
+        WHERE color LIKE '%blue%'
+
+""", conn1)
+#print(df_blue)
 
 ##### Part 3: Ordering and Limiting #####
 
@@ -45,16 +99,52 @@ pd.read_sql("SELECT * FROM dogs;", conn2)
 
 # STEP 6
 # Replace None with your code
-df_hungry = None
+df_hungry = pd.read_sql("""
+        SELECT name, age, breed
+        FROM dogs
+        WHERE hungry = True
+        ORDER BY age ASC
+
+""",conn2)
+""" REPLACE EVERY NaN IN THE DATAFRAME WITH None TO AVOID NaN!=None ERROR"""
+df_hungry = df_hungry.replace({np.nan: None})
+#print(df_hungry)
 
 # STEP 7
 # Replace None with your code
-df_hungry_ages = None
+df_hungry_ages = pd.read_sql("""
+        SELECT name, age, hungry
+        FROM dogs
+        WHERE age BETWEEN 2 AND 7 AND hungry = True
+        ORDER BY name ASC
+
+""", conn2)
+#print(df_hungry_ages)
 
 # STEP 8
 # Replace None with your code
-df_4_oldest = None
+""" CORRECT CODE ACCORDING TO INSTRUCTIONS """
+# df_4_oldest = pd.read_sql("""
+#         SELECT name, age, breed 
+#         FROM (
+#                 SELECT name, age, breed
+#                 FROM dogs
+#                 ORDER BY age DESC 
+#                 LIMIT 4              
+#         )
+#         ORDER BY breed ASC  
+        
 
+# """, conn2)
+#print(df_4_oldest)
+
+""" CORRECT CODE ACCORDING TO TEST EXPECTATION WITHOUT SORTING THE BREED ALPHABETICALLY """
+df_4_oldest = pd.read_sql("""
+    SELECT name, age, breed
+    FROM dogs
+    ORDER BY age DESC
+    LIMIT 4
+""", conn2)
 
 ##### Part 4: Aggregation #####
 
@@ -70,23 +160,44 @@ SELECT * FROM babe_ruth_stats; """, conn3)
 
 # STEP 9
 # Replace None with your code
-df_ruth_years = None
+df_ruth_years = pd.read_sql("""
+        SELECT COUNT(year)
+        FROM babe_ruth_stats
+
+""", conn3)
+#print(df_ruth_years)
 
 # STEP 10
 # Replace None with your code
-df_hr_total = None
-
+df_hr_total = pd.read_sql("""
+        SELECT SUM(HR)
+        FROM babe_ruth_stats
+                          
+""", conn3)
+#print(df_hr_total)
 
 ##### Part 5: Grouping and Aggregation #####
 
 # STEP 11
 # Replace None with your code
-df_teams_years = None
+df_teams_years = pd.read_sql("""
+        SELECT team, COUNT(year) AS number_years
+        FROM babe_ruth_stats
+        GROUP BY team
+                             
+""", conn3)
+#print(df_teams_years)
 
 # STEP 12
 # Replace None with your code
-df_at_bats = None
+df_at_bats = pd.read_sql("""
+        SELECT team, AVG(at_bats) AS average_at_bats
+        FROM babe_ruth_stats
+        GROUP BY team
+        HAVING average_at_bats > 200
 
+""", conn3)
+#print(df_at_bats)
 
 conn1.close()
 conn2.close()
